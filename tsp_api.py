db289d67bb82cedfd8938d877922c9c397fa1857
vagrant <vagrant@CloudNativeGo>
2025-05-23 10:07:16 +0000

    asd

--- tsp_api.py
+++ tsp_api.py
@@ -1,24 +1,49 @@
 from flask import Flask, request, jsonify
-import googlemaps
 from ortools.constraint_solver import pywrapcp, routing_enums_pb2
-import os
+from math import radians, sin, cos, sqrt, atan2
 
 app = Flask(__name__)
 
-gmaps = googlemaps.Client(key=os.environ["GOOGLE_API_KEY"])
+def haversine_distance(coord1, coord2):
+    # Koordináták (lat, lon)
+    lat1, lon1 = coord1
+    lat2, lon2 = coord2
+
+    # Föld sugara km-ben
+    R = 6371.0
+
+    # Radiánba váltás
+    phi1, phi2 = radians(lat1), radians(lat2)
+    dphi = radians(lat2 - lat1)
+    dlambda = radians(lon2 - lon1)
+
+    a = sin(dphi / 2)**2 + cos(phi1) * cos(phi2) * sin(dlambda / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    return R * c
 
 @app.route("/optimize", methods=["POST"])
 def optimize():
     data = request.get_json()
-    locations = data["locations"]
-    coords = [(loc["lat"], loc["lng"]) for loc in locations]
+    raw_locations = data["locations"]
 
-    matrix_result = gmaps.distance_matrix(coords, coords, mode="driving")
-    distance_matrix = [
-        [elem["distance"]["value"] for elem in row["elements"]]
-        for row in matrix_result["rows"]
+    # Tisztítás: konvertál float-ra és szűrés, ha hiányzik adat
+    coords = [
+        (float(loc["lat"]), float(loc["lng"]))
+        for loc in raw_locations
+        if loc.get("lat") not in [None, ""] and loc.get("lng") not in [None, ""]
     ]
 
+    if len(coords) < 2:
+        return jsonify({"error": "Legalább 2 érvényes koordináta szükséges."}), 400
+
+    # Távolságmátrix (km-ben)
+    distance_matrix = [
+        [int(haversine_distance(c1, c2) * 1000) for c2 in coords]
+        for c1 in coords
+    ]
+
+    # OR-Tools
     manager = pywrapcp.RoutingIndexManager(len(distance_matrix), 1, 0)
     routing = pywrapcp.RoutingModel(manager)
 
@@ -30,6 +55,7 @@ def optimize():
 
     params = pywrapcp.DefaultRoutingSearchParameters()
     params.first_solution_strategy = routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+
     solution = routing.SolveWithParameters(params)
 
     route = []
@@ -38,7 +64,7 @@ def optimize():
         while not routing.IsEnd(index):
             route.append(manager.IndexToNode(index))
             index = solution.Value(routing.NextVar(index))
-        route.append(manager.IndexToNode(index))
+        route.append(manager.IndexToNode(index))  # Visszatérés a kiindulóponthoz
 
     return jsonify({"route": route})
 
